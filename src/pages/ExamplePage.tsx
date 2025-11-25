6b3ebe7d66e6ae2ef5dd1fc1c014265c475c84d6
unknown <alexisramon1608@gmail.com>
2025-11-24 19:30:48 -0800

    little fix

--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -1,10 +1,3 @@
-import Section from '@/components/sections/Section'
-import RowSection from '@/components/sections/RowSection'
-import Partition from '@/components/sections/Partition'
-import ColumnSection from '@/components/sections/ColumnSection'
-import NavigationPartition from '@/components/sections/NavigationPartition'
-import WindowPanel from '@/components/WindowPanel'
-
 /**
  * Example page demonstrating horizontal partitioning
  */
